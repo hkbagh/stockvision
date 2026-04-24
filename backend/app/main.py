7fbdd15d1553a0365419a937e98082235bfe1c5a
Harekrishna Bagh <harekrishnabagh14@gmail.com>
2026-04-24 13:52:28 +0530

    Allow POST in CORS for admin endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -60,7 +60,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["GET"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
